14849edaff164c06aaa51f5c92a0103d62aa89f2
cluster05 <ajayakumbhar1234@gmail.com>
2021-05-24 18:34:32 +0530

    Hello world in graph-ql.

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql');
 
 const app = express();
+
+const schema = new GraphQLSchema({
+    // getting of data
+    query: new GraphQLObjectType({
+        name: 'helloworld',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            }
+        })
+
+    })
+})
+
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema
+}))
 
 app.listen(3000, () => {
     console.log(`listing on port 3000`)
